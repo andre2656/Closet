ba43f8158783ad7dd1449618accdc134dd58847d
andre2656 <44176906+andre2656@users.noreply.github.com>
2019-04-02 23:07:47 -0600

    Building Marketing Components

--- client/src/components/marketingSite/LiveChat.js
+++ client/src/components/marketingSite/LiveChat.js
@@ -1,2 +1,24 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import './MarketingSite.css';
+
+const LiveChat = () => {
+    let content = { marginTop: "70px" }
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-2" />
+                <div className="col-md-4" style={content}>
+                    <h2>Live Chat With a Stylist</h2>
+                    <p>Get all the fashion advice you've ever wanted from the comfort of your home. With a premium account, you can access a live stylist to discuss in app purchases!</p>
+                </div>
+                <div className="col-md-4">
+                    <img id="chat-image" src="images/LiveChat.jpg" />
+                </div>
+                <div className="col-md-2" />
+            </div>
+        </div>
+    )
+}
+
+export default LiveChat;
