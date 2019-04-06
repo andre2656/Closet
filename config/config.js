65f004a49e7260970e8a4327a9a0e5d16f506324
Andre Campbell <andrecampbell@Andres-MacBook-Pro.local>
2019-04-06 13:25:51 -0600

    Changed the production in config.js

--- config/config.js
+++ config/config.js
@@ -16,10 +16,6 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "Users_production",
-    "host": "127.0.0.1",
     "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
