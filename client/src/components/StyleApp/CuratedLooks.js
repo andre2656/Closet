12adcb836df68b7c607cad87bab07f6562fa8b75
Char Bray <cbray0@yahoo.com>
2019-04-06 09:45:10 -0700

    StyleApp Components Created

---
+++ client/src/components/StyleApp/CuratedLooks.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import './CuratedLooks.css';
+
+
+
+class CuratedLooks extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Curated Looks</h1>
+                </div>
+                <div className='row'>
+                    <div className='col-md-1' />
+                    <div className='col-md-5' className='card' id='pinCard'>
+                        <img src="https://i.pinimg.com/originals/58/04/f6/5804f689d3dccde403d330e291586c2f.png" class="card-img-top" />
+                    </div>
+                    <div className='col-md-5' className='card' id='pinCard'>
+                        <img src="https://i.pinimg.com/originals/c0/6d/7a/c06d7af98ba7c6d2fa8d97dddd45aa44.png" class="card-img-top" />
+                    </div>
+                    <div className='col-md-1' />
+                </div>
+                <div className='row'>
+                    <div className='col-md-1' />
+                    <div className='col-md-5' className='card' id='pinCard'>
+                        <img src="https://i.pinimg.com/originals/58/04/f6/5804f689d3dccde403d330e291586c2f.png" class="card-img-top" />
+                    </div>
+                    <div className='col-md-5' className='card' id='pinCard'>
+                        <img src="https://i.pinimg.com/originals/c0/6d/7a/c06d7af98ba7c6d2fa8d97dddd45aa44.png" class="card-img-top" />
+                    </div>
+                    <div className='col-md-1' />
+                </div>
+                <div className='row'>
+                    <div className='col-md-1' />
+                    <div className='col-md-5' className='card' id='pinCard'>
+                        <img src="https://i.pinimg.com/originals/58/04/f6/5804f689d3dccde403d330e291586c2f.png" class="card-img-top" />
+                    </div>
+                    <div className='col-md-5' className='card' id='pinCard'>
+                        <img src="https://i.pinimg.com/originals/c0/6d/7a/c06d7af98ba7c6d2fa8d97dddd45aa44.png" class="card-img-top" />
+                    </div>
+                    <div className='col-md-1' />
+                </div>
+            </div>
+        );
+    }
+};
+
+export default CuratedLooks;
---
+++ client/src/components/StyleApp/Quizzes.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Quizzes = () => {
+    
+    return (
+        <div>
+            <div>
+                <h1>Quizzes</h1>
+            </div>
+            <div className="bd-example">
+            <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="https://cdn.shopify.com/s/files/1/0868/1504/files/bf_grande.png?v=1533662108" className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Fashion Style</h5>
+                            <p>Learn more about what your ideal fashion style would be?</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://patternobserver.com/wp-content/uploads/2014/05/IMG-12-palette-picking.png" className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Coloring</h5>
+                            <p>What is your ideal color palette.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://wordpress-mnei139eijehv5w.netdna-ssl.com/wp-content/uploads/2017/03/the-mod-the-modist-modest-icons-march-issue-modest-fashion.png" className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Fashion Icon</h5>
+                            <p>Which fashion icon do you most emmulate?</p>
+                        </div>
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        
+        </div>
+
+    )
+}
+
+export default Quizzes;
