309668e117369a58fce53017b7e95810dff0e94c
Christina Lavingia <christinalavingia@gmail.com>
2019-04-08 14:38:32 -0700

    Adjust text overlay on easy purchases.

--- client/src/components/MarketingSite/EasyPurchases.js
+++ client/src/components/MarketingSite/EasyPurchases.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import './MarketingSite.css';
+import './EasyPurchases.css';
 
 
 const EasyPurchases = () => {
@@ -9,10 +9,8 @@ const EasyPurchases = () => {
         <div className="container flex-container">
             <div className="row">
                 <div className="col-md-2" />
-                <div className="col-md-4">
-                    <img id="shop-image" src="images/online-purchase.jpg" />
-                </div>
-                <div className="col-md-4" id="easy-purchase-text" style={content}>
+                <img className="col-md-4" id="shop-image" src="images/shop-dresses.png" />
+                <div className="col-md-4" id="shop-text" style={content}>
                     <h2>Make Easy Purchases</h2>
                     <p>See something you love? Be able to make a purchase with the click of a button from within our app!</p>
                 </div>
