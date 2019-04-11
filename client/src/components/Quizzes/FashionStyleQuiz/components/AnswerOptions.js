c2f2c6e06cb9b183b6dceaf27e4bf0f85abc1296
Char Bray <cbray0@yahoo.com>
2019-04-11 16:56:42 -0700

    css for quizes updated

--- client/src/components/Quizzes/FashionStyleQuiz/components/AnswerOptions.js
+++ client/src/components/Quizzes/FashionStyleQuiz/components/AnswerOptions.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
- 
- 
+
+
 function AnswerOptions(props) {
 	return (
-		<li className="answerOption">
+		<div className="answerOption">
 			<input
 				type="radio"
 				className="radioCustomButton"
@@ -15,21 +15,19 @@ function AnswerOptions(props) {
 				disabled={props.answer}
 				onChange={props.onAnswerSelected}
 			/>
- 
-			<label className="radioCustomLabel" htmlFor={props.answerType}>
-				{props.answerContent}
-			</label>
- 
-		</li>
+				<label className="radioCustomLabel" htmlFor={props.answerType}>
+					{props.answerContent}
+				</label>
+		</div>
 	);
 }
- 
+
 AnswerOptions.propTypes = {
 	answerType: PropTypes.string.isRequired,
 	answerContent: PropTypes.string.isRequired,
 	answer: PropTypes.string.isRequired,
 	OnAnswerSelected: PropTypes.string.isRequired,
- 
+
 };
- 
+
 export default AnswerOptions;
--- client/src/components/Quizzes/FashionStyleQuiz/components/QuestionCount.js
+++ client/src/components/Quizzes/FashionStyleQuiz/components/QuestionCount.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function QuestionCount(props) {
 	return (
 		<div className="questionCount">
-			Question<span>{props.counter}</span> of <span>{props.total}</span>
+			Question <span>{props.counter}</span> of <span>{props.total}</span>
 		</div>
  
 	);
