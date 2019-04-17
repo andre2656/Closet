d5f130e3004e1b0bfccd888f95c2b2af16a1fd08
Char Bray <cbray0@yahoo.com>
2019-04-16 18:46:37 -0700

    FS quiz results component switch case added

--- client/src/components/Quizzes/FashionStyleQuiz/components/Result.js
+++ client/src/components/Quizzes/FashionStyleQuiz/components/Result.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Classic from './resultOptions/Classic';
+import Trendy from './resultOptions/Trendy';
+import Sporty from './resultOptions/Sporty';
+import Urban from './resultOptions/Urban';
+import Bohemian from './resultOptions/Bohemian';
+import Dramatic from './resultOptions/Dramatic';
+import Romantic from './resultOptions/Romantic';
+// import results from './api/results';
 
 function Result(props) {
+
+	const getComponent = () => {
+		switch (props.quizResult) {
+
+			case "Classic": return <Classic />;
+			case "Trendy": return <Trendy />;
+			case "Dramatic": return <Dramatic />;
+			case "Romantic": return <Romantic />;
+			case "Sporty": return <Sporty />;
+			case "Bohemian": return <Bohemian />;
+			case "Urban": return <Urban />;
+			default: return <h1>No style match</h1>;
+		};
+	};
+
+
 	return (
-		// <ReactCSSTransitionGroup
-		// 	className="container result"
-		// 	component ="div"
-		// 	transitionName="fade"
-		// 	transitionEnterTimeout={800}
-		// 	transitionLeaveTimeout={500}
-		// 	transitionAppear
-		// 	transitionAppearTimeout={500}
-		// >
+
 		<div>
-			<div>
-				You prefer  <strong> {props.quizResult}</strong>!
-				</div>
-				<br/>
+
+			{getComponent()}
+
+			<br />
 			<div>
 				<Link className="return" to="/app"><button type="button" className="btn btn-dark">Return</button></Link>
 			</div>
 		</div>
-		// </ReactCSSTransitionGroup>
-	);
-}
+
+	)
+
+};
 
 Result.propTypes = {
 	quizResult: PropTypes.string.isRequired,
