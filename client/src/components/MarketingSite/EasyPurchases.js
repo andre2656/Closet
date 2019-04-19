03ec48ef5b3239997c9cd5c2fa26729a3e9a6322
ChristinaLavingia <christinalavingia@gmail.com>
2019-04-18 22:22:06 -0700

    Change image and column sizes.

--- client/src/components/MarketingSite/EasyPurchases.js
+++ client/src/components/MarketingSite/EasyPurchases.js
@@ -7,13 +7,13 @@ const EasyPurchases = () => {
     return (
         <div className="container flex-container">
             <div className="row">
-                <div className="col-md-2" />
-                <img className="col-md-4" id="shop-image" src="images/shop.png" alt= 'shop' />
-                <div className="col-md-4" id="shop-text" style={content}>
+                <div className="col-md-1" />
+                <img className="col-md-5" id="shop-image" src="images/shop.png" alt= 'shop' />
+                <div className="col-md-5" id="shop-text" style={content}>
                     <h2>Make Easy Purchases</h2>
-                    <p>See something you love? Be able to identify items from a photo and make purchases with the click of a button!</p>
+                    <p>See a look you need to have? Be able to identify items from a photo and make purchases with the click of a button!</p>
                 </div>
-                <div className="col-md-2" />
+                <div className="col-md-1" />
             </div>
         </div>
     )
--- client/src/components/MarketingSite/LiveChat.js
+++ client/src/components/MarketingSite/LiveChat.js
@@ -6,15 +6,15 @@ const LiveChat = () => {
     return (
         <div className="container flex-container">
             <div className="row">
-                <div className="col-md-2" />
-                <div className="col-md-4" id="chat-text" style={{marginTop: "130px"}}>
+                <div className="col-md-1" />
+                <div className="col-md-5" id="chat-text" style={{marginTop: "130px"}}>
                     <h2>Live Chat With a Stylist</h2>
-                    <p>Get all the fashion advice you've ever wanted from the comfort of your home. With a premium account, you can access a live stylist to discuss in-app purchases!</p>
+                    <p>Get all the fashion advice you've ever wanted from the comfort of your home with a premium account!</p>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-5">
                     <img id="chat-image" src="images/stylist.jpg" alt= 'chat' />
                 </div>
-                <div className="col-md-2" />
+                <div className="col-md-1" />
             </div>
         </div>
     )
