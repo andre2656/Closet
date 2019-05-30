a2801498ee56a16f8f7c931870545038aae251f6
Andre <andre2656@gmail.com>
2019-05-30 16:37:52 -0600

    Updated board urls

--- client/src/components/StyleApp/StyleRecognition/StyleRecognition.js
+++ client/src/components/StyleApp/StyleRecognition/StyleRecognition.js
@@ -7,39 +7,39 @@ class StyleRecognition extends React.Component {
         data: {},
         img: 'https://www.davidjones.com/images/assetimages/blog/Happening/Denim-street-style-trend-hero.jpg'
     };
-    componentDidMount (){
-        console.log('call once')
-        // this.callOnce()
-    }
-    callOnce = () => {
-        if ($('#pictures').val() === 'empty') {
-            // this.getImages();
-        }
-    }   
+    // componentDidMount (){
+    //     console.log('call once')
+    //     // this.callOnce()
+    // }
+    // callOnce = () => {
+    //     if ($('#pictures').val() === 'empty') {
+    //         // this.getImages();
+    //     }
+    // }   
     // componentWillReceiveProps = (props) => {
     //     this.CheckImg(props.img)
     // }
       
-    CheckImg = (img) => {
-        if (this.state.img !== img){
-            this.setState({ img: img},
-                () => {
-                    if (this.state.img) {
-                        // this.getImages();
-                    }
-                    else {
-                        console.log('no img')
-                    }
-                })
-        }else{
-            console.log('Same Img')
-        }
-    }
+//     CheckImg = (img) => {
+//         if (this.state.img !== img){
+//             this.setState({ img: img},
+//                 () => {
+//                     if (this.state.img) {
+//                         // this.getImages();
+//                     }
+//                     else {
+//                         console.log('no img')
+//                     }
+//                 })
+//         }else{
+//             console.log('Same Img')
+//         }
+//     }
 
-    componentDidMount = () => {
-        this.setState({ img: this.props.img });
-        // this.getImages();
-    };
+//     componentDidMount = () => {
+//         this.setState({ img: this.props.img });
+//         // this.getImages();
+//     };
     render() {
         return (
           <div>
